0d9e16490e3430b6b8f4787ec8b3ff6ef5edce29
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-03-10 20:19:11 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 44 ms, Memory: 3008 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+#define INF 1000000000
+
+int N, r, c;
+int sum[501], matrix[501][2], dp[501][501];
+
+int main()
+{
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> r >> c;
+		matrix[i][0] = r;
+		matrix[i][1] = c;
+	}
+
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 1; i + j <= N; j++)
+		{
+			dp[j][i + j] = INF;
+			for (int k = j; k <= i + j; k++)
+			{
+				dp[j][i + j] = min(dp[j][i + j], dp[j][k] + dp[k + 1][i + j] + matrix[j][0] * matrix[k][1] * matrix[i+j][1]);
+			}
+		}
+
+	}
+
+	cout << dp[1][N];
+
+
+}
