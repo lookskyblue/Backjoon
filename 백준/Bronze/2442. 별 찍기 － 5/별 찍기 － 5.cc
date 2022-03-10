532ff109802978199b9694e1f1290df2b8fd3d93
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-03-10 20:29:05 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+			cout << " ";
+
+		for (int j = 0; j < (i + 1) * 2 - 1; j++)
+			cout << "*";
+
+		cout << endl;
+	}
+
+	return 0;
+}
