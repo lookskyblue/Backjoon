a5eae8e463242402df01f92111f7e7eb9bd13d84
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-03-13 19:40:49 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int arr[9];
+
+void DFS(int idx, int cnt)
+{
+    if(cnt == M)
+    {
+        for(int i = 0; i < M; i++)
+            cout << arr[i] << " ";
+        cout << '\n';
+        return;
+    }
+    
+    for(int i = idx; i <= N; i++)
+    {
+        arr[cnt] = i;
+        DFS(i, cnt + 1);
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    DFS(1, 0);
+
+    return 0;
+}
