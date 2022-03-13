888963b7ae1ed7565f327b1128bcfa3fdfeaa755
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-03-13 19:51:41 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int N, M;
+vector<int> v;
+int arr[8];
+
+void Input()
+{
+	cin >> N >> M; 
+	for (int i = 0; i < N; i++)
+	{
+		int val;
+		cin >> val;
+		v.push_back(val);
+	}
+
+	sort(v.begin(), v.end());
+}
+
+void DFS(int idx, int cnt)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < cnt; i++)
+			cout << arr[i] << " ";
+		cout << '\n';
+
+		return;
+	}
+
+	for (int i = idx; i < N; i++)
+	{
+		arr[cnt] = v[i];
+		DFS(i + 1, cnt + 1);
+	}
+}
+
+int main()
+{
+	Input();
+	DFS(0, 0);
+}
