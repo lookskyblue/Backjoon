f5029b6274b7c8fbf421b8ad1205229f869c43ec
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-03-15 00:46:04 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -25,51 +25,50 @@ void Input()
 void GetNextDirPos(int next_dir, int& next_x, int& next_y)
 {
 	if (next_dir == 0) next_x--;
-	if (next_dir == 1) next_y++;
-	if (next_dir == 2) next_x++;
-	if (next_dir == 3) next_y--;
+	else if (next_dir == 1) next_y++;
+	else if (next_dir == 2) next_x++;
+	else next_y--;
 }
 
 bool IsCleared(int next_x, int next_y)
 {
-	if (map[next_x][next_y] == 1 || cleared[next_x][next_y] == true) return true;
+	if (map[next_x][next_y] == 1 || cleared[next_x][next_y] == true) 
+		return true;
 
 	return false;
 }
 
 void Solve()
 {
-	int rot_cnt = 0;
-
 	while (1)
 	{
-		rot_cnt = 0;
+		cleared[pos_x][pos_y] = true;
+		clear_cnt++;
 
-		if (cleared[pos_x][pos_y] == false)
-		{
-			cleared[pos_x][pos_y] = true;
-			clear_cnt++;
-		}
-
+		int rot_cnt = 0;
 		int next_dir = (dir + 4 - 1) % 4;
 		int next_x = 0, next_y = 0;
+
 		GetNextDirPos(next_dir, next_x, next_y);
-		
+
 		while (IsCleared(pos_x + next_x, pos_y + next_y) == true)
 		{
 			rot_cnt++;
 			dir = (dir + 4 - 1) % 4;
-			
-			if (rot_cnt == 4) 
+
+			if (rot_cnt == 4)
 			{
-				int next_x = 0, next_y = 0;
-				GetNextDirPos((dir+4-2) % 4, next_x, next_y);
+				int nx = 0, ny = 0;
+				GetNextDirPos((dir + 4 - 2) % 4, nx, ny);
 
-				if (map[pos_x + next_x][pos_y + next_y] == 1)
+				if (map[pos_x + nx][pos_y + ny] == 1)
+				{
+					cout << clear_cnt;
 					return;
+				}
 
-				pos_x += next_x;
-				pos_y += next_y;
+				pos_x += nx;
+				pos_y += ny;
 				rot_cnt = 0;
 			}
 
@@ -87,12 +86,11 @@ void Solve()
 
 int main()
 {
-	ios_base::sync_with_stdio(0); 
+	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
 	Input();
 	Solve();
-	cout << clear_cnt;
 
 	return 0;
 }
