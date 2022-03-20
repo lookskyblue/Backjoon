a01d154ba7af05ccee06954797488dfc13388f84
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-03-21 01:08:39 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -10,20 +10,18 @@ int N[100000];
 
 int BinarySearch(int left, int right, int value)
 {
+	if (left == right) return 0;
 	if (N[left] == value) return 1;
 	if (N[right] == value) return 1;
 
 	int mid = (left + right) / 2;
-	
-	if (N[mid] == value) return 1;
 
-	if (left == right) return 0;
+	if (N[mid] == value) return 1;
 
 	int is_any = 0;
 
 	if (value < N[mid])		 is_any += BinarySearch(left, mid, value);
 	else if (N[mid] < value) is_any += BinarySearch(mid + 1, right, value);
-
 	return is_any;
 }
 
@@ -32,8 +30,7 @@ void Solve()
 	int n, m;
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
-		cin >> N[i];
+	for (int i = 0; i < n; i++) cin >> N[i];
 
 	sort(N, N + n);
 	cin >> m;
@@ -41,8 +38,8 @@ void Solve()
 	for (int i = 0; i < m; i++)
 	{
 		int x;
-		cin >> x;
 
+		cin >> x;
 		cout << BinarySearch(0, n - 1, x) << '\n';
 	}
 }
@@ -51,7 +48,7 @@ int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	
+
 	Solve();
 
 	return 0;
