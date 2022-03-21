c146c2a7eebc426567cce97fe4a6f768690c4d46
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-03-22 02:03:32 +0900

    [Silver III] Title: 스택 수열, Time: 24 ms, Memory: 3016 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -7,27 +7,28 @@ using namespace std;
 
 int N, K;
 int arr[100000];
-queue<string> op;
+string ans;
 
 void Solve()
 {
 	int N;
 	cin >> N;
 
-	for (int i = 0; i < N; i++) cin >> arr[i];
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
 
 	int x = 1;
 	int idx = 0;
 	stack<int> s;
 	
 	s.push(x);
-	op.push("+");
+	ans += "+";
 
 	while (1)
 	{
 		if (s.top() == arr[idx])
 		{
-			op.push("-");
+			ans += "-";
 			s.pop();
 
 			if (++idx == N) break;
@@ -35,27 +36,25 @@ void Solve()
 
 		if (s.empty() == true)
 		{
-			op.push("+");
+			ans += "+";
 			s.push(++x);
 		}
 
 		if (s.top() < arr[idx])
 		{
 			s.push(++x);
-			op.push("+");
+			ans += "+";
 		}
 
-		if (s.top() > arr[idx])
+		else if (s.top() > arr[idx])
 		{
 			cout << "NO";
 			return;
 		}
 	}
 
-	int size = op.size();
-
-	for (int i = 0; i < size; i++, op.pop())
-		cout << op.front() << '\n';
+	for (int i = 0; i < ans.size(); i++)
+		cout << ans[i] << '\n';
 }
 
 int main()
