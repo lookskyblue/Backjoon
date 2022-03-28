00cbf445de265ac800a98d956e0d12ab8c9b32ce
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-03-29 02:17:49 +0900

    [Silver III] Title: 에디터, Time: 44 ms, Memory: 2948 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -3,46 +3,70 @@
 #include <string>
 #include <queue>
 #include <list>
+#include <stack>
 
 using namespace std;
 
-int cursor;
-
 void Solve()
 {
+	stack<char> left, right;
 	string str;
 	int M;
 
 	cin >> str >> M;
 
-	list<char> ls(str.begin(), str.end());
-	list<char>::iterator now = ls.end();
+	for (int i = 0; i < str.size(); i++)
+		left.push(str[i]);
 
 	for (int i = 0; i < M; i++)
 	{
 		char order;
 		cin >> order;
 
-		if (order == 'L' && now != ls.begin())
-			now--;
+		if (order == 'L')
+		{
+			if (left.empty() == false)
+			{
+				right.push(left.top());
+				left.pop();
+			}
+		}
 
-		else if (order == 'D' && now != ls.end())
-			now++;
+		else if (order == 'D')
+		{
+			if (right.empty() == false)
+			{
+				left.push(right.top());
+				right.pop();
+			}
+		}
 
-		else if (order == 'B' && now != ls.begin())
-			now = ls.erase(--now);
+		else if (order == 'B')
+		{
+			if (left.empty() == false)
+				left.pop();
+		}
 
-		else if(order == 'P')
+		else if (order == 'P')
 		{
 			char alpha;
 			cin >> alpha;
 
-			ls.insert(now, alpha);
+			left.push(alpha);
 		}
 	}
 
-	for (list<char>::iterator itr = ls.begin(); itr != ls.end(); itr++)
-		cout << *itr;
+	while (left.empty() == false)
+	{
+		right.push(left.top());
+		left.pop();
+	}
+
+	while (right.empty() == false)
+	{
+		cout << right.top();
+		right.pop();
+	}
 }
 
 int main()
