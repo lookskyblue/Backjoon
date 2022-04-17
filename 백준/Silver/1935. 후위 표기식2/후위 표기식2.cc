4a1aad73cd38e985122c34446d695f8fd1ccf7a2
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-04-18 00:23:01 +0900

    [Silver III] Title: 후위 표기식2, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <map>
+#include <stack>
+#include <cctype>
+
+using namespace std;
+
+map<char, int> alpha_map;
+stack<double> s;
+
+double Calculate(double a, double b, char letter)
+{
+	if (letter == '*') return a * b;
+	else if (letter == '/') return a / b;
+	else if (letter == '+') return a + b;
+	else if (letter == '-') return a - b;
+}
+
+void Solve(string str)
+{
+	for (int i = 0; i < str.size(); i++)
+	{
+		char letter = str[i];
+
+		if (letter >= 'A' && letter <= 'Z') // 알파벳이면
+		{
+			s.push(alpha_map[letter]);
+		}
+		else // 연산자면
+		{
+			double a, b;
+
+			b = s.top();
+			s.pop();
+			a = s.top();
+			s.pop();
+
+			double result = Calculate(a, b, letter);
+
+			s.push(result);
+		}
+	}
+
+	//cout << fixed;
+	//cout.precision(2);
+	
+	printf("%.2f", s.top());
+	//cout << s.top();
+}
+
+void Input()
+{
+	string str;
+	int N;
+	
+	cin >> N;
+	cin >> str;
+
+	for (int i = 0; i < N; i++)
+	{
+		int x;
+		cin >> x;
+
+		alpha_map.insert({ 'A' + i, x });
+	}
+
+	Solve(str);
+}
+
+int main()
+{
+	Input();
+
+	return 0;
+}
