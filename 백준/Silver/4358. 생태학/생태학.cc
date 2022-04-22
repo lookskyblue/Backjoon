4bf59f1169bd76dbb051055011496c564dd4f20a
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-04-23 01:07:23 +0900

    [Silver I] Title: 생태학, Time: 168 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+map<string, int> tree_m;
+
+void Input()
+{
+	cout << fixed;
+
+	int n = 0;
+	int total_tree = 0;
+	string tree;
+
+	while (getline(cin, tree))
+	{
+		map<string, int>::iterator itr = tree_m.find(tree);
+
+		if (itr != tree_m.end())
+			itr->second++;
+
+		else
+			tree_m.insert({ tree, 1 });
+
+		total_tree++;
+
+		//if (getchar() == 'a') break;
+	}
+
+	int tree_m_size = tree_m.size();
+
+	for (map<string, int>::iterator itr = tree_m.begin(); itr != tree_m.end(); itr++)
+	{
+		double percentage = ((double)itr->second / total_tree) * 100;
+		percentage = round(percentage * 10000) / 10000;
+
+		cout.precision(4);
+
+		cout << itr->first << " " << percentage << '\n';
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	Input();
+
+	return 0;
+}
