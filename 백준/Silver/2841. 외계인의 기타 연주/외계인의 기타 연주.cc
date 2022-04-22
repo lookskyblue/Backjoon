01c55a559372eda174b1c7063f931fb36ac16c01
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-04-23 00:03:56 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 84 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+
+using namespace std;
+
+stack<int> s[6];
+int answer;
+
+void Input()
+{
+	int N, P;
+	cin >> N >> P;
+
+	for (int i = 0; i < N; i++)
+	{
+		int idx, plat;
+		cin >> idx >> plat;
+
+		idx--;
+
+		if (s[idx].empty() == true) // 스택이 비었으면 
+		{
+			s[idx].push(plat);
+			answer++;
+		}
+
+		else // 스택이 비어있지 않다면
+		{
+			if (s[idx].top() < plat) // 최신 플랫이 더 높다면 
+			{
+				s[idx].push(plat);
+				answer++;
+			}
+
+			else if(s[idx].top() > plat) // 최신 플랫이 더 낮다면
+			{
+				while (s[idx].empty() == false) 
+				{
+					if (s[idx].top() > plat)
+					{
+						s[idx].pop();
+						answer++;
+
+						if (s[idx].empty() == true)
+						{
+							s[idx].push(plat);
+							answer++;
+							break;
+						}
+					}
+
+					else if (s[idx].top() == plat)
+						break;
+
+					else
+					{
+						s[idx].push(plat);
+						answer++;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	cout << answer;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	Input();
+
+	return 0;
+}
