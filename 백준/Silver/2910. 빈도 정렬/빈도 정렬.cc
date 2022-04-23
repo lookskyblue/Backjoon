cc258ee9044c13abe2d50d43acc75e095084ab08
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-04-24 01:14:48 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+map<int, pair<int, int>> frequency;
+
+bool cmp(const pair<int, pair<int, int>> &a, const pair<int, pair<int, int>> &b)
+{
+	if (a.second.first < b.second.first) return false;
+	else if (a.second.first == b.second.first)
+	{
+		if (a.second.second > b.second.second)
+			return false;
+	}
+
+	return true;
+}
+
+void Solve()
+{
+	/*for (map<int, pair<int, int>>::iterator itr = frequency.begin(); itr != frequency.end(); itr++)
+	{
+		cout << "값: " << itr->first << ", 빈도: " << itr->second.first << ", 순서: " << itr->second.second << '\n';
+	}*/
+
+	vector<pair<int, pair<int, int>>> v;
+
+	for (auto it = frequency.begin(); it != frequency.end(); it++)
+	{
+		v.push_back(make_pair(it->first, it->second));
+	}
+
+	sort(v.begin(), v.end(), cmp);
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		int loop_count = v[i].second.first;
+
+		for (int j = 0; j < loop_count; j++)
+		{
+			cout << v[i].first << ' ';
+		}
+	}
+}
+
+void Input()
+{
+	int N, C;
+	cin >> N >> C;
+
+	for (int i = 0; i < N; i++)
+	{
+		int x;
+		cin >> x;
+
+		map<int, pair<int, int>>::iterator itr = frequency.find(x);
+
+		if (itr == frequency.end())
+			frequency.insert({ x, {1, i} });
+		else
+			itr->second.first++;
+	}
+
+	Solve();
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); 
+	cout.tie(0);
+
+	Input();
+
+	return 0;
+}
