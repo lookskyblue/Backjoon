1bc4ac7d914d37f52e3391ebd1db7516e4d57cfc
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-04-24 19:30:28 +0900

    [Silver IV] Title: 차집합, Time: 636 ms, Memory: 25520 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.cc
+++ 백준/Silver/1822. 차집합/차집합.cc
@@ -6,24 +6,11 @@
 #include <stack>
 #include <map>
 #include <set>
+#include <vector>
 
 using namespace std;
 
-set<int> as, bs, diffs;
-
-void Solve()
-{
-	for (set<int>::iterator itr = as.begin(); itr != as.end(); itr++)
-	{
-		if (bs.find(*itr) == bs.end())
-			diffs.insert(*itr);
-	}
-
-	cout << diffs.size() << '\n';
-
-	for (set<int>::iterator itr = diffs.begin(); itr != diffs.end(); itr++)
-		cout << *itr << ' ';
-}
+set<int> s;
 
 void Input()
 {
@@ -35,7 +22,7 @@ void Input()
 		int x;
 		cin >> x;
 
-		as.insert(x);
+		s.insert(x);
 	}
 
 	for (int i = 0; i < B; i++)
@@ -43,16 +30,20 @@ void Input()
 		int x;
 		cin >> x;
 
-		bs.insert(x);
+		if (s.find(x) != s.end())
+			s.erase(x);
 	}
 
-	Solve();
+	cout << s.size() << '\n';
+
+	for (set<int>::iterator itr = s.begin(); itr != s.end(); itr++)
+		cout << *itr << ' ';
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
-	cin.tie(0); 
+	cin.tie(0);
 	cout.tie(0);
 
 	Input();
