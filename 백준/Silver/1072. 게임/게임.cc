16b365291dca96318ed3a0c13e85c838407fb397
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-05-04 23:53:07 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.cc
+++ 백준/Silver/1072. 게임/게임.cc
@@ -16,29 +16,17 @@ int ans = 1000000001;
 
 void Solve() 
 {
-	//Z = (((double)Y / X) * 100);
-	//Z = int(((double)Y / X) * 100);
 	int Z = (100 * Y) / X;
 
 	int start = 1;
 	int mid = 0;
 	int end = 1000000000;
 	
-	//cout << "((double)Y / X) * 100 : " << ((double)Y / X) * 100 << "\n";
-	//cout << "(int)(((double)Y / X) * 100) : " << (int)(((double)Y / X) * 100) << "\n";
-	//double tmp_z = ((double)Y / X) * 100;
-	//cout << "tmp_z: " << tmp_z << "\n";
-	//cout << "tmp_z: " << (int)(floor(tmp_z)) << "\n";
-	
-	//cout << "Z: " << Z << "\n";
-
 	while (start <= end)
 	{
 		mid = (start + end) / 2;
 
 		if (Z < (100*(Y + mid)) / (X + mid))
-		//if ((((((double)(Y + mid)) / (X + mid)) * 100)) - Z > (double)1)
-		//if (abs(((((double)Y+mid) / (X+mid)) * 100) - Z) >= double(1))
 		{
 			ans = min(ans, mid);
 			end = mid - 1;
