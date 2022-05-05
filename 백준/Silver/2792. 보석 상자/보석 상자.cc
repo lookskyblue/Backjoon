7e598c8968c7bd517baa13e825f61619139ab671
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-05-05 17:50:33 +0900

    [Silver II] Title: 보석 상자, Time: 148 ms, Memory: 3456 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.cc
+++ 백준/Silver/2792. 보석 상자/보석 상자.cc
@@ -1,94 +1,42 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <set>
-#include <vector>
-#include <map>
-#include <cstring>
-
-using namespace std;
-
-int N, M;
-priority_queue<int> pq;
-int ans;
-
-bool CanShare(int jealousy)
-{
-	priority_queue<int> copy_pq = pq;
-	int shared_child_cnt = 0;
-
-	while (copy_pq.empty() == false)
-	{
-		int jeal = copy_pq.top();
-		copy_pq.pop();
-
-		int q = jeal / jealousy;
-		int r = jeal % jealousy;
-
-		shared_child_cnt += q;
-		
-		if (r != 0)
-			shared_child_cnt++;
-
-		if (shared_child_cnt > N)
-			return false;
-	}
-
-	return true;
-}
-
-void Solve()
-{
-	int low = 1;
-	int mid = 0;
-	int high = pq.top();
-
-	while (low <= high)
-	{
-		mid = (low + high) / 2;
-		//cout << "MID: " << mid << '\n';
-
-		if (CanShare(mid) == true)
-		{
-			//cout << "Can Share!" << '\n';
-			high = mid - 1;
-			ans = mid;
-		}
-		else
-		{
-			//cout << "Can Not Share!" << '\n';
-			low = mid + 1;
-		}
-	}
-
-	cout << ans;
-}
-
-void Input()
-{
-	cin >> N >> M;
-
-	for (int i = 0; i < M; i++)
-	{
-		int x;
-		cin >> x;
-
-		pq.push(x);
-	}
-
-	Solve();
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
-	Input();
-
-	return 0;
+#include <stdio.h>
+long long j[300010] = {};
+ 
+int main(void){
+ 
+    long long  N, M;
+    long long result=1000000000;
+    long long max = 0;
+    scanf("%lld %lld", &N, &M);
+ 
+    for (int i = 0; i < M; i++){
+        scanf("%lld", &j[i]);
+        if (max < j[i])
+            max = j[i];
+    }
+ 
+    long long left = 1, right = max;
+    
+    while (left <= right){
+        long long mid = (left + right) / 2;
+        long long tempT = 0;
+        
+        for (int i = 0; i < M; i++)
+        {
+            tempT += j[i] / mid;
+            if (j[i]%mid != 0)
+                tempT++;
+        }
+ 
+        if (tempT <= N){
+            right = mid - 1;
+            if (mid<result)
+            result = mid;
+        }
+        else
+            left = mid + 1;
+ 
+    }
+ 
+    printf("%lld\n", result);
+ 
 }
