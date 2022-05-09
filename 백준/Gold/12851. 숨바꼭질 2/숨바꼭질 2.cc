23cead9e0bebbf33933676bf8d52c2a2fc1cc0c9
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-05-09 21:18:55 +0900

    [Gold V] Title: 숨바꼭질 2, Time: 8 ms, Memory: 3460 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <vector>
+#include <map>
+#include <cstring>
+#define INF 987654321
+#define MAX 100001
+
+using namespace std;
+
+int N, K;
+int maze[MAX];
+int dist[MAX];
+
+int min_time = -1;
+int min_time_count;
+
+void Solve()
+{
+	dist[N] = 0;
+	queue<int> q;
+	q.push(N);
+
+	while (q.empty() == false)
+	{
+		int cur_x = q.front();
+		q.pop();
+
+		if (cur_x == K)
+		{
+			if (min_time != dist[K])
+			{
+				min_time = dist[K];
+				min_time_count = 1;
+			}
+
+			else if (min_time == dist[K])
+				min_time_count++;
+		}
+
+		int one_step_back = cur_x - 1;
+		int one_step_forward = cur_x + 1;
+		int teleport = cur_x * 2;
+
+		if (0 <= one_step_back && one_step_back < MAX)
+		{
+			if (dist[one_step_back] >= dist[cur_x] + 1)
+			{
+				dist[one_step_back] = dist[cur_x] + 1;
+				q.push(one_step_back);
+			}
+		}
+
+		if (0 <= one_step_forward && one_step_forward < MAX)
+		{
+			if (dist[one_step_forward] >= dist[cur_x] + 1)
+			{
+				dist[one_step_forward] = dist[cur_x] + 1;
+				q.push(one_step_forward);
+			}
+		}
+
+		if (0 <= teleport && teleport < MAX)
+		{
+			if (dist[teleport] >= dist[cur_x] + 1)
+			{
+				dist[teleport] = dist[cur_x] + 1;
+				q.push(teleport);
+			}
+		}
+	}
+
+	cout << dist[K] << '\n' << min_time_count;
+}
+
+void Input()
+{
+	cin >> N >> K;
+
+	for (int i = 0; i < MAX; i++)
+		dist[i] = INF;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	Input();
+	Solve();
+
+	return 0;
+}
