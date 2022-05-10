8bbc54dec7c8fe304372a8650d1b2d6afb4f53b2
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-05-11 01:20:19 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+
+void Solve()
+{
+	string str;
+
+	while (getline(cin, str))
+		cout << str << '\n';
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	Solve();
+
+	return 0;
+}
