d8d67a9f236cb1c87b011a26e8d262ca2b21c0f6
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-07-21 22:16:04 +0900

    [level 2] Title: 타겟 넘버, Time: 0.28 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,30 +1,24 @@
-#include <string>
-#include <vector>
-
+#include<iostream>
+#include<vector>
+#include<queue>
 using namespace std;
 
 int answer;
 
-void DFS(vector<int> &numbers, int sum, int now_idx, int &target)
+void DFS(vector<int> numbers, int sum, int idx, int target)
 {
-    if (now_idx == numbers.size())
-    {
-        if (sum == target) answer++;
-        return;
-    }
+	if (numbers.size() - 1 == idx)
+	{
+		if ((sum + numbers[idx]) == target)	answer++;
+		if ((sum - numbers[idx]) == target)	answer++;
+		return;
+	}
 
-    DFS(numbers, sum + numbers[now_idx], now_idx+1, target);
-    DFS(numbers, sum - numbers[now_idx], now_idx+1, target);
+	DFS(numbers, sum + numbers[idx], idx + 1, target);
+	DFS(numbers, sum - numbers[idx], idx + 1, target);
 }
 
 int solution(vector<int> numbers, int target) {
-
-    answer = 0;
-
-    int sum = 0;
-    int now_idx = 0;
-
-    DFS(numbers, sum, now_idx, target);
-
-    return answer;
+	DFS(numbers, 0, 0, target);
+	return answer;
 }
