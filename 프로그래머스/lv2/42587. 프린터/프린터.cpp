84bff2dc801a61bf7bdcaa6de2f6d91ff53fbbd7
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-07-21 22:29:28 +0900

    [level 2] Title: 프린터, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.cpp
+++ 프로그래머스/lv2/42587. 프린터/프린터.cpp
@@ -1,51 +1,37 @@
-#include <string>
-#include <vector>
-
+#include<iostream>
+#include<vector>
+#include<queue>
 using namespace std;
+
 int solution(vector<int> priorities, int location) {
-    
-    vector<pair<int, int>> job_queue;
+    int answer = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>> pq;
+    queue<pair<int, int>> q;
 
     for (int i = 0; i < priorities.size(); i++)
     {
-        pair<int, int> p = pair<int, int>(priorities[i], i);
-
-        job_queue.push_back(p);
+        pq.push(pair<int, int>(priorities[i], i));
+        q.push(pair<int, int>(priorities[i], i));
     }
 
-    int answer = 0;
-    int print_count = 0;
-
-    while (job_queue.empty() == false)
+    while (q.empty() == false)
     {
-        pair<int, int> job = *job_queue.begin();
-
-        bool is_push_back = false;
-
-        for (int i = 1; i < job_queue.size(); i++)
+        if (q.front().first < pq.top().first)
         {
-            if (job.first < job_queue[i].first)
-            {
-                job_queue.erase(job_queue.begin());
-                job_queue.push_back(job);
-                is_push_back = true;
-
-                break;
-            }
+            q.push(q.front());
+            q.pop();
         }
 
-        if (is_push_back == false)
+        else
         {
-            int pr_location = (*job_queue.begin()).second;
+            answer++;
 
-            job_queue.erase(job_queue.begin());
-            print_count++;
-
-            if (pr_location == location)
-            {
-                answer = print_count;
+            if (q.front().second == location)
                 break;
-            }
+
+            pq.pop(); 
+            q.pop();
         }
     }
 
