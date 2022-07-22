782dc41c42b68b6eb518423dd4db4b8437257bae
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-07-22 12:56:33 +0900

    [level 2] Title: 문자열 압축, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.cpp
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 #include<queue>
 #include<string>
-#include<algorithm>
+
 using namespace std;
 
 int solution(string s) {
@@ -10,17 +10,22 @@ int solution(string s) {
 
     for (int i = 1; i <= s.size() / 2; i++)
     {
-        int cnt = 1;
-        string prev = s.substr(0, i);
         string tmp = "";
+        string prev = s.substr(0, i);
+        int cnt = 1;
 
         for (int j = i; j < s.size(); j += i)
         {
-            if (prev == s.substr(j, i)) cnt++;
-
+            if (prev == s.substr(j, i))
+            {
+                cnt++;
+            }
             else
             {
-                if (cnt > 1) tmp += to_string(cnt);
+                if (cnt > 1)
+                {
+                    tmp += to_string(cnt);
+                }
 
                 tmp += prev;
                 prev = s.substr(j, i);
@@ -28,9 +33,14 @@ int solution(string s) {
             }
         }
 
-        if (cnt > 1) tmp += to_string(cnt);
+        if (cnt > 1)
+        {
+            tmp += to_string(cnt);
+        }
+
         tmp += prev;
-        if (tmp.size() < answer) answer = tmp.size();
+
+        if (answer > tmp.size()) answer = tmp.size();
     }
 
     return answer;
