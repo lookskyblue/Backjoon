c09252d80f5d8bc807c3dd8d75ce512700e3b48c
Mojave_Ghost <71778475+lookskyblue@users.noreply.github.com>
2022-08-29 00:15:18 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 896 ms, Memory: 212040 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+set<string> strSet;
+
+void Solve()
+{
+	string str;
+	cin >> str;
+
+	for (int i = 0; i < str.length() ; i++)
+	{
+		for (int j = 0; j < str.length() && (j+i) < str.length(); j++)
+		{
+			string tmp = str.substr(j, i + 1);
+			strSet.insert(tmp);
+		}
+	}
+
+	cout << strSet.size();
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	Solve();
+}
